26e05f1c8dfc0efc5444b175ebcd2284b957dd66
vvergara <victorvergara.c99@gmail.com>
2023-09-08 15:02:31 -0300

    Se modifica categoría del producto por una clave foranea

--- ProductosApp/models.py
+++ ProductosApp/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
+class Categoria(models.Model):
+    descripcion = models.CharField(max_length=50)
+    activa      = models.BooleanField()
+
+    def __str__(self):
+        return f"Categoría: {self.descripcion}"
+
 class Producto(models.Model):
     descripcion   = models.CharField(max_length=100)
-    categoria     = models.CharField(max_length=50)
+    # categoria     = models.CharField(max_length=50)
+    categoria     = models.ForeignKey(Categoria, on_delete=models.CASCADE, null=True, blank=True)
     precio        = models.IntegerField()
     oferta        = models.BooleanField()
     precio_oferta = models.IntegerField()
@@ -11,12 +19,6 @@ class Producto(models.Model):
     def __str__(self):
         return f"Producto: {self.descripcion}"
 
-class Categoria(models.Model):
-    descripcion = models.CharField(max_length=50)
-    activa      = models.BooleanField()
-
-    def __str__(self):
-        return f"Categoría: {self.descripcion}"
     
 class ProductoImg(models.Model):
     imagen   = models.ImageField(upload_to="products")
